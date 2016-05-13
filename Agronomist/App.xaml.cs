f0dc72c45ea4e39dc664b6ad82da86723cb12343
Manish Parekh <t3hmun_at_gmail>
2016-05-13 17:57:02 +0100

    Nike agronomist ui because it needed to.

--- Agronomist/App.xaml.cs
+++ Agronomist/App.xaml.cs
@@ -1,75 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
 namespace Agronomist
 {
-    using System.Threading.Tasks;
-    using Windows.ApplicationModel.Activation;
-    using Windows.UI.Xaml;
-    using Microsoft.Data.Entity;
-    using Models;
-    using Services.SettingsServices;
-    using Template10.Common;
-    using Template10.Controls;
-    using Views;
-
-    sealed partial class App : BootStrapper
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
     {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
         public App()
         {
-            InitializeComponent();
-            SplashFactory = e => new Splash(e);
-
-            var settings = SettingsService.Instance;
-            RequestedTheme = settings.AppTheme;
-            CacheMaxDuration = settings.CacheMaxDuration;
-            // Shell back button is a bad idea in an app that likes to hife the window chrtom altogether.
-            ShowShellBackButton = false;
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
         }
 
-
-        public override async Task OnInitializeAsync(IActivatedEventArgs args)
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-            // Code here is sued to replace the default root-frame of the Nav service with a shell including the hamburger menu.
-            if (!(Window.Current.Content is ModalDialog))
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
             {
-                // create a new frame 
-                var nav = NavigationServiceFactory(BackButton.Attach, ExistingContent.Include);
-                // create modal root
-                Window.Current.Content = new ModalDialog
-                {
-                    DisableBackButtonWhenModal = true,
-                    Content = new Shell(nav),
-                    ModalContent = new Busy()
-                };
+                this.DebugSettings.EnableFrameRateCounter = true;
             }
-            await Task.CompletedTask;
-        }
+#endif
+            Frame rootFrame = Window.Current.Content as Frame;
 
-        /// <summary>
-        ///     Used to execute long running startup things.
-        ///     Is only run when starting up the app fresh, not when restoring.
-        /// </summary>
-        /// <param name="startKind"></param>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
-        {
-            // Long-running startup tasks go here.
-            await InitialiseDatabase();
-            NavigationService.Navigate(typeof(MainPage));
-            await Task.CompletedTask;
-        }
-
-        /// <summary>
-        ///     Runs any database initialisation and maintenance tasks that must be completed before the app starts.
-        /// </summary>
-        /// <returns>Awaitable task.</returns>
-        private async Task InitialiseDatabase()
-        {
-            // Make sure the database is created and migrated up-to-date.
-            await Task.Run(() =>
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
             {
-                using (var x = new MainDbContext())
-                    x.Database.Migrate();
-            });
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Ensure the current window is active
+                Window.Current.Activate();
+            }
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
         }
     }
 }
