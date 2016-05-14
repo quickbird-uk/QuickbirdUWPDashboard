193d38c15bc4ac5f8c3f451d30c5f2a73ee45a5e
Manish Parekh <t3hmun@gmail.com>
2016-05-14 13:24:32 +0100

    Created unit test proj, close #8

---
+++ NetLibTests/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿namespace NetLibTests
+{
+    using Xunit;
+
+    public class SimpleTest
+    {
+        /// <summary>
+        ///     Test for the test runner.
+        /// </summary>
+        [Fact]
+        public void TrueTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
