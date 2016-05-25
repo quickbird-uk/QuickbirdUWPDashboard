d11b285485d714d1a65dd99954fff034462c0575
Manish Parekh <t3hmun@gmail.com>
2016-05-25 16:30:35 +0100

    WIP creating vm for sensors.

---
+++ Agronomist/ViewModels/SensorViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agronomist.ViewModels
+{
+    using Windows.ApplicationModel.Core;
+    using Windows.UI.Core;
+    using DatabasePOCOs;
+    using Util;
+
+    class SensorViewModel : ViewModelBase
+    {
+        private readonly Sensor _sensorPoco;
+        private Action<IEnumerable<Messenger.SensorReading>> _dataUpdater;
+        private CoreDispatcher _dispatcher;
+
+        public SensorViewModel(Sensor sensorPoco)
+        {
+            _dispatcher = CoreApplication.MainView.CoreWindow.Dispatcher;
+            _sensorPoco = sensorPoco;
+            _dataUpdater = readings =>
+            {
+                var mostRecent = readings.Where(r => r.SensorId == _sensorPoco.ID).Max(r => r.Timestamp);
+                await _dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => SetValue(mostRecent.))
+            };
+            Messenger.Instance.NewSensorDataPoint.Subscribe(_dataUpdater);
+        }
+    }
+}
