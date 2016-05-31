4981a3888791308928ca2740a0ab6648d5924dab
t3hmun <t3hmun_at_gmail>
2016-05-31 16:24:28 +0100

    Temporary setting to graphing nav.

--- Agronomist/ViewModels/ShellViewModel.cs
+++ Agronomist/ViewModels/ShellViewModel.cs
@@ -2,6 +2,8 @@
 {
     using System.Linq;
     using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Views;
 
     public class ShellViewModel : ViewModelBase
     {
@@ -10,6 +12,12 @@
         private void Update()
         {
             
+        }
+
+        public void NavToGraphingView()
+        {
+            
+            ((Frame) Window.Current.Content).Navigate(typeof(GraphingView));
         }
         
     }
--- Agronomist/Views/Shell.xaml.cs
+++ Agronomist/Views/Shell.xaml.cs
@@ -5,12 +5,15 @@
     using Windows.UI.Xaml;
     using Windows.UI.Xaml.Controls;
     using Windows.UI.Xaml.Navigation;
+    using ViewModels;
 
     /// <summary>
     ///     An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class Shell
     {
+        public ShellViewModel ViewModel { get; } = new ShellViewModel();
+
         public Shell()
         {
             InitializeComponent();
