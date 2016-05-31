fc3517649334c6bc8877de7ec8d6708b249d30de
Vladimir Akopyan <vladimir@akopyan.me>
2016-05-31 15:08:58 +0100

    Trying to integrate the Analytics View

---
+++ Agronomist/ViewModels/AnalyticsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Agronomist.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Agronomist.ViewModels
+{
+    public class AnalyticsViewModel
+    {
+
+        public AnalyticsViewModel()
+        {
+            var db = new MainDbContext(); 
+
+            //var data = db.CropCycles.
+        }
+
+
+    }
+}
