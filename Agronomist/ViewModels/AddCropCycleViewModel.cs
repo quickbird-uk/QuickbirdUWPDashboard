817b89a39a4bed3aac61305ef27c744484d43fbd
Vladimir Akopyan <vladimir@akopyan.me>
2016-06-02 21:52:34 +0100

    Completed re #77

--- Agronomist/ViewModels/AddCropCycleViewModel.cs
+++ Agronomist/ViewModels/AddCropCycleViewModel.cs
@@ -7,7 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Agronomist.Models;
-
+using DatabasePOCOs;
+using Agronomist.Util;
 
 namespace Agronomist.ViewModels
 {
@@ -15,19 +16,51 @@ namespace Agronomist.ViewModels
     {
         MainDbContext _db = new MainDbContext();
 
-        private HashSet<PlaceTuple> _placeList = new HashSet<PlaceTuple>();
+        private ObservableCollection<PlaceTuple> _placeList = new ObservableCollection<PlaceTuple>();
         private PlaceTuple _chosenPlace = null;
-        private bool _chosenIsVacant = false; 
+        private bool _chosenIsVacant = false;
+        private string _userEnteredCropType = string.Empty;
+        private List<string> _suggestedList = new List<string>(); 
+        private List<KeyValuePair<string, CropType>> _CropTypeCache = new List<KeyValuePair<string, CropType>>(); 
         private Action<string> updateEvent; 
+
+        
 
         public AddCropCycleViewModel()
         {
-            var places = _db.Locations.Where(loc => loc.Devices.Count > 0).Include(loc => loc.CropCycles).ToList();
+            updateEvent = UpdateData;
+            Messenger.Instance.TablesChanged.Subscribe(updateEvent); 
+            UpdateData(string.Empty); 
+        }
+
+
+        /// <summary>
+        /// Update Data event, runs every time 
+        /// </summary>
+        /// <param name="something"></param>
+        private async void UpdateData(string something)
+        {
+            var places = await _db.Locations.Where(loc => loc.Devices.Count > 0).Include(loc => loc.CropCycles).ToListAsync();
+            var cropTypeCache = await _db.CropTypes.ToListAsync();
+            foreach (CropType cropType in cropTypeCache)
+            {
+                if (_CropTypeCache.Any(ct => ct.Key.Equals(cropType.Name.ToLower())) == false)
+                {
+                    var pair = new KeyValuePair<string, CropType>(cropType.Name.ToLower(),
+                        cropType);
+                    _CropTypeCache.Add(pair);
+                }
+            }
 
             foreach (Location loc in places)
             {
-                PlaceTuple tuple = new PlaceTuple
-                {  Location = loc }; 
+                PlaceTuple tuple =
+                    PlaceList.FirstOrDefault(ct => ct.Location.Equals(loc));
+                if (tuple == null)
+                {
+                    tuple = new PlaceTuple { Location = loc };
+                    PlaceList.Add(tuple);
+                }
 
                 CropCycle runningCropCycle = loc.CropCycles.FirstOrDefault(cc => cc.EndDate == null);
                 if (runningCropCycle != null)
@@ -38,18 +71,18 @@ namespace Agronomist.ViewModels
                 else
                 {
                     tuple.DisplayName = loc.Name + " - Avaliable";
-                    tuple.IsVacant = true; 
+                    tuple.IsVacant = true;
                 }
-                PlaceList.Add(tuple);
+
+               
             }
-            OnPropertyChanged("PlaceList"); 
+            OnPropertyChanged("PlaceList");
         }
 
-
-
-
-
-        public HashSet<PlaceTuple> PlaceList
+        /// <summary>
+        /// List of places to be displayed to the user. 
+        /// </summary>
+        public ObservableCollection<PlaceTuple> PlaceList
         { get {
                 return _placeList; }
             private set {
@@ -70,6 +103,37 @@ namespace Agronomist.ViewModels
             }
         }
 
+        /// <summary>
+        /// CropType Chosen By the user. Set By UI
+        /// </summary>
+        public string UserCropType
+        {
+            get { return _userEnteredCropType; }
+            set
+            {
+                _userEnteredCropType = value;                
+                SuggestedList = _CropTypeCache.Where(ct => ct.Key
+                .Contains(_userEnteredCropType.ToLower()))
+                .Select(Ct => Ct.Value.Name).ToList(); 
+            }
+        }
+
+        /// <summary>
+        /// Suggested Crop List for the suggestion box. 
+        /// </summary>
+        public List<string> SuggestedList
+        {
+            get { return _suggestedList; }
+            set
+            {
+                _suggestedList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Indicates if the chosen Location is avaliable for a new cropRun to be created
+        /// </summary>
         public Boolean ChosenIsVacant
         {
             get { return _chosenIsVacant; }
@@ -79,6 +143,46 @@ namespace Agronomist.ViewModels
             }
         }
 
+        public string CropVariety { get; set; }
+
+        public async void CreateNewCropRun()
+        {
+            ChosenIsVacant = false; 
+            Settings settings = new Settings();
+            CropType cropType = _CropTypeCache.FirstOrDefault(ct => ct.Key.Equals(UserCropType.ToLower())).Value
+                ?? new CropType
+                {
+                    Name = UserCropType,
+                    Approved = false,
+                    CreatedAt = DateTimeOffset.Now,
+                    CreatedBy = settings.CredStableSid
+                };
+            CropCycle cropCycle = new CropCycle
+            {
+                ID = Guid.NewGuid(),
+                Name = string.Empty,
+                Yield = 0,
+                CropType = cropType,
+                CropTypeName = cropType.Name,
+                CropVariety = CropVariety,
+                LocationID = _chosenPlace.Location.ID,
+                Location = _chosenPlace.Location,
+                CreatedAt = DateTimeOffset.Now,
+                UpdatedAt = DateTimeOffset.Now,
+                StartDate = DateTimeOffset.Now,
+                EndDate = null,
+                Deleted = false,
+                Version = new byte[32]
+            };
+            _db.CropCycles.Add(cropCycle);
+
+            await _db.SaveChangesAsync();
+            await Messenger.Instance.TablesChanged.Invoke(string.Empty); 
+        }
+
+
+
+
         /// <summary>
         /// Used as a grouping for UI to blabber about
         /// </summary>
@@ -86,7 +190,13 @@ namespace Agronomist.ViewModels
         {
             private string _displayName;
             private Location _location;
-            private bool _isVacant; 
+            private bool _isVacant;
+
+            //public override bool Equals(object obj)
+            //{
+            //    PlaceTuple other = obj as PlaceTuple; 
+            //    return other?._displayName.ToLower().Equals(_displayName.ToLower()) ?? false;
+            //}
 
 
             public string DisplayName {
