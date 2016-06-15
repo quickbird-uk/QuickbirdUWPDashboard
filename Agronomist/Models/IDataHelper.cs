70d6cd65821b56fb87d847e7406faa17cf28f875
t3hmun <t3hmun_at_gmail>
2016-06-15 20:42:35 +0100

    Database helper interface wip.

---
+++ Agronomist/Models/IDataHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agronomist.Models
+{
+    using DatabasePOCOs;
+    using DatabasePOCOs.User;
+
+    interface IDataHelper
+    {
+        Sensor GetSensor(Guid id);
+
+        IEnumerable<CropCycle> GetCropCycles();
+    }
+}
