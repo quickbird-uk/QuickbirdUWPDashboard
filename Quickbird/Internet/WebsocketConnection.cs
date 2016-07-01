33e1cd582b4e71360108a071cf80e0351d5c6edd
Vladimir Akopyan <vladimir@akopyan.me>
2016-07-01 21:16:59 +0100

    WIP Now we have a persistent websocket connection on the client, that reconnect and has exponential back-off re #146

--- Quickbird/Internet/WebsocketConnection.cs
+++ Quickbird/Internet/WebsocketConnection.cs
@@ -18,140 +18,229 @@ namespace Quickbird.Internet
     {
         public static WebsocketConnection Instance { get; } = new WebsocketConnection();
         private static MessageWebSocket _webSocket = new MessageWebSocket(); //it is laso the subject of lock
-        private static DataWriter messageWriter;
+        private static DataWriter _messageWriter;
 
         public event PropertyChangedEventHandler PropertyChanged;
+        const string SocketCloseMessage = "AppIsSuspending"; 
+
+        public static Timer _ReconnectTimer;
+        int _reconnectionAttempt; //Used for exponenetial backoff timer 
+
+        private static long _AppRunning = 1; //1 stands for running, 0 for suspended;  
 
 
 
 
-        Task _task;
 
-        uint reconnectionAttempt; //Used for exponenetial backoff timer 
-
-        public WebsocketConnection(){
-
-            Debug.WriteLine("WebsocketStarting"); 
-            _task = Task.Run(Connect);
-            _task.ContinueWith(async (Task it) =>
-            { await SendAsync("Test 1!!"); }); 
-        }
+        // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
+        private readonly Action<TaskCompletionSource<object>> _resumeAction;
+        // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
+        private readonly Action<TaskCompletionSource<object>> _suspendAction;
 
 
-        private void SocketClosed(IWebSocket sender, WebSocketClosedEventArgs args)
+
+        //Connected property, bindable directly from the UI
+        private long _connected = 0;
+        public bool Connected
         {
-            Connected = false;
-            CleanUp(); 
-        }
-
-
-        private async Task<bool> SendAsync(string toSend)
-        {
-            //string jsonData = JsonConvert.SerializeObject(toSend);
-            messageWriter.WriteString(toSend);
-            try
+            get
             {
-                // Send the data as one complete message.
-                await messageWriter.StoreAsync();
+                return _connected == 1;
             }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-                return false;
-            }
-
-            return true; 
         }
 
-        private void MessageRecieved(MessageWebSocket sender, MessageWebSocketMessageReceivedEventArgs args)
-        {
-            using (DataReader reader = args.GetDataReader())
-            {
-                reader.UnicodeEncoding = UnicodeEncoding.Utf8;
 
+        public WebsocketConnection()
+        {
+
+            Debug.WriteLine("Websocket Starting");
+
+            _resumeAction = Resume;
+            _suspendAction = Suspend;
+            Messenger.Instance.Suspending.Subscribe(_suspendAction);
+            Messenger.Instance.Resuming.Subscribe(_resumeAction);
+
+            _ReconnectTimer = new Timer(Connect, null, 1000, Timeout.Infinite);
+        }
+
+
+        /// <summary>
+        /// A best-effor sender of data
+        /// </summary>
+        /// <param name="toSend"></param>
+        /// <returns></returns>
+        public async Task<bool> SendAsync(object toSend)
+        {
+            string jsonData = JsonConvert.SerializeObject(toSend);
+
+            if (Interlocked.Read(ref _connected) == 1)
+            {
+                _messageWriter.WriteString(jsonData);
                 try
                 {
-                    string read = reader.ReadString(reader.UnconsumedBufferLength);
-                    Debug.WriteLine(read);
+                    // Send the data as one complete message.
+                    await _messageWriter.StoreAsync();
+                    return true; 
                 }
                 catch (Exception ex)
                 {
                     Debug.WriteLine(ex.Message);
+                    SocketClosed(); 
+                    return false;
                 }
+            }
+            else
+                return false; 
+        }
+
+        private void MessageRecieved(MessageWebSocket sender, MessageWebSocketMessageReceivedEventArgs args)
+        {
+            
+            if (SocketMessageType.Utf8 == args.MessageType)
+            {
+                try
+                {
+                    using (DataReader reader = args.GetDataReader())
+                    {
+                        reader.UnicodeEncoding = UnicodeEncoding.Utf8;
+                        string read = reader.ReadString(reader.UnconsumedBufferLength);
+                        Debug.WriteLine(read);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    SocketClosed(); 
+                }                
             }
         }
 
-        private async Task Connect()
+        /// <summary>
+        /// Overload for the Timer
+        /// </summary>
+        /// <param name="state">is null</param>
+        private async void Connect(object state)
         {
+            _ReconnectTimer.Dispose();
+            bool success = await Connect();
+        }
+
+        private async Task<bool> Connect()
+        {
+            if (Settings.Instance.CredsSet == false)
+                return false; 
+
             if (_webSocket == null)
                 _webSocket = new MessageWebSocket(); 
 
             _webSocket.Control.MessageType = SocketMessageType.Utf8;
-            _webSocket.MessageReceived += MessageRecieved;
             _webSocket.Closed += SocketClosed;
+            _webSocket.MessageReceived += MessageRecieved;
+
 
             var tokenHeader = "X-ZUMO-AUTH";
             var creds = Creds.FromUserIdAndToken(Settings.Instance.CredUserId, Settings.Instance.CredToken);
-            _webSocket.SetRequestHeader(tokenHeader, creds.Token); 
+            _webSocket.SetRequestHeader(tokenHeader, creds.Token);
 
-            if (Settings.Instance.CredsSet)
-            {
-                try
-                {
-                    Uri uri = new Uri("wss://ghapi46azure.azurewebsites.net/api/Websocket");
-                    
-                    
-                    await _webSocket.ConnectAsync(uri);
-                    messageWriter = new DataWriter(_webSocket.OutputStream);
-                    Connected = true; 
-                }
-                catch
-                {
-                    CleanUp(); 
-                }
-            }            
-        }
-
-        public void Close()
-        {
-            Connected = false; 
-
+            
             try
             {
-                _webSocket?.Close(1000, "Closed due to user request.");
+                Uri uri = new Uri("wss://ghapi46azure.azurewebsites.net/api/Websocket");
+                await _webSocket.ConnectAsync(uri);
+                _messageWriter = new DataWriter(_webSocket.OutputStream);
             }
-            catch (Exception ex)
+            catch
             {
-                Debug.WriteLine(ex.Message);
+                Debug.WriteLine("Websocket connection failed");
+                SocketClosed();
+                return false;
             }
-            CleanUp(); 
+
+            _reconnectionAttempt = 0;
+
+            Interlocked.Exchange(ref _connected, 1);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Connected"));
+
+            Debug.WriteLine("Websocket connected");
+            return true;
+        }
+
+
+
+        /// <summary>
+        /// These methids are linked to the messenger
+        /// </summary>
+        /// <param name="taskCompletionSource"></param>
+        private void Resume(TaskCompletionSource<object> taskCompletionSource)
+        {
+            Interlocked.Exchange(ref _AppRunning, 1);
+            _ReconnectTimer = new Timer(Connect, null, 0, Timeout.Infinite);
+            taskCompletionSource.SetResult(null);
+        }
+
+        private void Suspend(TaskCompletionSource<object> taskCompletionSource)
+        {
+            Interlocked.Exchange(ref _AppRunning, 0);
+            SocketClosed();
+            taskCompletionSource.SetResult(null);
+        }
+
+        /// <summary>
+        /// Unclear what hte hell this does
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void SocketClosed(IWebSocket sender, WebSocketClosedEventArgs args)
+        {
+            if (args.Reason == SocketCloseMessage)
+            {
+                //means the app closed it, but so what? 
+            }
+
+            //SocketClosed();
         }
 
         /// <summary>
         /// Call after the connection is closed. Get ready for new connection
         /// </summary>
-        private void CleanUp()
+        private void SocketClosed()
         {
-            messageWriter?.DetachStream();
-            messageWriter?.Dispose();
-            messageWriter = null;
+            _ReconnectTimer?.Dispose();
+            Interlocked.Exchange(ref _connected, 0);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Connected"));
+
+            try
+            {
+                _webSocket?.Close(1000, SocketCloseMessage);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            _messageWriter?.DetachStream();
+            _messageWriter?.Dispose();
+            _messageWriter = null;
 
             _webSocket?.Dispose();
             _webSocket = null;
+
+            if(Interlocked.Read(ref _AppRunning) == 0)
+            {
+                //Don't need to do anything
+            }
+            else
+            {              
+                _reconnectionAttempt++;
+
+                //Multiple should not exceed value of 6
+                int timeMultiple = _reconnectionAttempt > 6 ? _reconnectionAttempt : 6;
+
+                int reconDelay = (int)Math.Round(Math.Pow(_reconnectionAttempt, 2) * 1000);
+
+                _ReconnectTimer = new Timer(Connect, null, reconDelay, Timeout.Infinite);
+            }
         }
 
-        //Connected property, bindable directly from the UI
-        private bool _connected = false;
-        public bool Connected {
-            get
-            {
-                return _connected;
-            }
-            set
-            {
-                _connected = value; 
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Connected"));
-            }
-        }
     }
 }
