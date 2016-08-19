23ee6df12307971c024cd8e692dc6a9eaa873a34
mun <t3hmun@gmail.com>
2016-08-19 13:05:11 +0100

    Fixes #6  Names fit on cards nestly now.

--- Quickbird/ViewModels/LiveCardViewModel.cs
+++ Quickbird/ViewModels/LiveCardViewModel.cs
@@ -277,7 +277,7 @@
         public void Update(Sensor poco)
         {
             Units = poco.SensorType.Param.Unit;
-            UnitName = poco.SensorType.Place.Name + " " + poco.SensorType.Param.Name;
+            UnitName = poco.SensorType.Place.Name + Environment.NewLine + poco.SensorType.Param.Name;
             //TODO: Status = poco.AlertStatus
         }
 
