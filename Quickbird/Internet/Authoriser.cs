b8f87200ab3c2b28bd1fb3f39af8543dc27a7a7c
t3hmun <t3hmun@gmail.com>
2016-10-27 18:03:32 +0100

    Started class for creating login-flow.

---
+++ Quickbird/Internet/Authoriser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quickbird.Internet
+{
+    using Windows.Web.Http;
+    using Windows.Web.Http.Filters;
+
+    public static class Authoriser
+    {
+        public static bool Login(string email, string password)
+        {
+            var client = new HttpClient(new HttpBaseProtocolFilter {AllowUI = false});
+        }
+    }
+}
