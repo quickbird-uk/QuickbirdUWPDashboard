a43d1f8707afe4df6fabf998439d3dbdf4100e0b
t3hmun <t3hmun@gmail.com>
2016-11-03 14:20:36 +0000

    Added db migration.

---
+++ Quickbird/Migrations/20161103141839_initial.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quickbird.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CropTypes",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: true),
+                    Deleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CropTypes", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parameters",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Unit = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parameters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Placements",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Placements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subsystems",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subsystems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SensorTypes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Autoincrement", true),
+                    ParameterId = table.Column<long>(nullable: false),
+                    PlacementId = table.Column<long>(nullable: false),
+                    SubsystemId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorTypes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SensorTypes_Parameters_ParameterId",
+                        column: x => x.ParameterId,
+                        principalTable: "Parameters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SensorTypes_Placements_PlacementId",
+                        column: x => x.PlacementId,
+                        principalTable: "Placements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SensorTypes_Subsystems_SubsystemId",
+                        column: x => x.SubsystemId,
+                        principalTable: "Subsystems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    PersonId = table.Column<Guid>(nullable: true),
+                    UpdatedAt = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Locations_People_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CropCycles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(nullable: false),
+                    CropTypeName = table.Column<string>(nullable: true),
+                    Deleted = table.Column<bool>(nullable: false),
+                    EndDate = table.Column<DateTimeOffset>(nullable: true),
+                    LocationId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(nullable: false),
+                    Yield = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CropCycles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CropCycles_CropTypes_CropTypeName",
+                        column: x => x.CropTypeName,
+                        principalTable: "CropTypes",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CropCycles_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    LocationId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    SerialNumber = table.Column<Guid>(nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Devices_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sensors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    DeviceId = table.Column<Guid>(nullable: false),
+                    Enabled = table.Column<bool>(nullable: false),
+                    SensorTypeId = table.Column<long>(nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sensors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sensors_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sensors_SensorTypes_SensorTypeId",
+                        column: x => x.SensorTypeId,
+                        principalTable: "SensorTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SensorsHistory",
+                columns: table => new
+                {
+                    SensorId = table.Column<Guid>(nullable: false),
+                    UtcDate = table.Column<DateTime>(nullable: false),
+                    LocationId = table.Column<Guid>(nullable: false),
+                    RawData = table.Column<byte[]>(nullable: true),
+                    UploadedAt = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorsHistory", x => new { x.SensorId, x.UtcDate });
+                    table.ForeignKey(
+                        name: "FK_SensorsHistory_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SensorsHistory_Sensors_SensorId",
+                        column: x => x.SensorId,
+                        principalTable: "Sensors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorTypes_ParameterId",
+                table: "SensorTypes",
+                column: "ParameterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorTypes_PlacementId",
+                table: "SensorTypes",
+                column: "PlacementId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorTypes_SubsystemId",
+                table: "SensorTypes",
+                column: "SubsystemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CropCycles_CropTypeName",
+                table: "CropCycles",
+                column: "CropTypeName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CropCycles_LocationId",
+                table: "CropCycles",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_LocationId",
+                table: "Devices",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Locations_PersonId",
+                table: "Locations",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sensors_DeviceId",
+                table: "Sensors",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sensors_SensorTypeId",
+                table: "Sensors",
+                column: "SensorTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorsHistory_LocationId",
+                table: "SensorsHistory",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorsHistory_SensorId",
+                table: "SensorsHistory",
+                column: "SensorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CropCycles");
+
+            migrationBuilder.DropTable(
+                name: "SensorsHistory");
+
+            migrationBuilder.DropTable(
+                name: "CropTypes");
+
+            migrationBuilder.DropTable(
+                name: "Sensors");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "SensorTypes");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Parameters");
+
+            migrationBuilder.DropTable(
+                name: "Placements");
+
+            migrationBuilder.DropTable(
+                name: "Subsystems");
+
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
---
+++ Quickbird/Migrations/QbDbContextModelSnapshot.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Quickbird.Models;
+
+namespace Quickbird.Migrations
+{
+    [DbContext(typeof(QbDbContext))]
+    partial class QbDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+
+            modelBuilder.Entity("Qb.Poco.Global.CropType", b =>
+                {
+                    b.Property<string>("Name");
+
+                    b.Property<DateTimeOffset>("CreatedAt");
+
+                    b.Property<Guid?>("CreatedBy");
+
+                    b.Property<bool>("Deleted");
+
+                    b.HasKey("Name");
+
+                    b.ToTable("CropTypes");
+                });
+
+            modelBuilder.Entity("Qb.Poco.Global.Parameter", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("Unit");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Parameters");
+                });
+
+            modelBuilder.Entity("Qb.Poco.Global.Placement", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Placements");
+                });
+
+            modelBuilder.Entity("Qb.Poco.Global.SensorType", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<long>("ParameterId");
+
+                    b.Property<long>("PlacementId");
+
+                    b.Property<long>("SubsystemId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("ParameterId");
+
+                    b.HasIndex("PlacementId");
+
+                    b.HasIndex("SubsystemId");
+
+                    b.ToTable("SensorTypes");
+                });
+
+            modelBuilder.Entity("Qb.Poco.Global.Subsystem", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Subsystems");
+                });
+
+            modelBuilder.Entity("Qb.Poco.Person", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTimeOffset>("CreatedAt");
+
+                    b.Property<DateTimeOffset>("UpdatedAt");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("People");
+                });
+
+            modelBuilder.Entity("Qb.Poco.User.CropCycle", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTimeOffset>("CreatedAt");
+
+                    b.Property<string>("CropTypeName");
+
+                    b.Property<bool>("Deleted");
+
+                    b.Property<DateTimeOffset?>("EndDate");
+
+                    b.Property<Guid>("LocationId");
+
+                    b.Property<string>("Name");
+
+                    b.Property<DateTimeOffset>("StartDate");
+
+                    b.Property<DateTimeOffset>("UpdatedAt");
+
+                    b.Property<double>("Yield");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("CropTypeName");
+
+                    b.HasIndex("LocationId");
+
+                    b.ToTable("CropCycles");
+                });
+
+            modelBuilder.Entity("Qb.Poco.User.Device", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTimeOffset>("CreatedAt");
+
+                    b.Property<bool>("Deleted");
+
+                    b.Property<Guid>("LocationId");
+
+                    b.Property<string>("Name");
+
+                    b.Property<Guid>("SerialNumber");
+
+                    b.Property<DateTimeOffset>("UpdatedAt");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("LocationId");
+
+                    b.ToTable("Devices");
+                });
+
+            modelBuilder.Entity("Qb.Poco.User.Location", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTimeOffset>("CreatedAt");
+
+                    b.Property<bool>("Deleted");
+
+                    b.Property<string>("Name");
+
+                    b.Property<Guid?>("PersonId");
+
+                    b.Property<DateTimeOffset>("UpdatedAt");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("PersonId");
+
+                    b.ToTable("Locations");
+                });
+
+            modelBuilder.Entity("Qb.Poco.User.Sensor", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTimeOffset>("CreatedAt");
+
+                    b.Property<bool>("Deleted");
+
+                    b.Property<Guid>("DeviceId");
+
+                    b.Property<bool>("Enabled");
+
+                    b.Property<long>("SensorTypeId");
+
+                    b.Property<DateTimeOffset>("UpdatedAt");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("DeviceId");
+
+                    b.HasIndex("SensorTypeId");
+
+                    b.ToTable("Sensors");
+                });
+
+            modelBuilder.Entity("Qb.Poco.User.SensorHistory", b =>
+                {
+                    b.Property<Guid>("SensorId");
+
+                    b.Property<DateTime>("UtcDate");
+
+                    b.Property<Guid>("LocationId");
+
+                    b.Property<byte[]>("RawData");
+
+                    b.Property<DateTimeOffset>("UploadedAt");
+
+                    b.HasKey("SensorId", "UtcDate");
+
+                    b.HasIndex("LocationId");
+
+                    b.HasIndex("SensorId");
+
+                    b.ToTable("SensorsHistory");
+                });
+
+            modelBuilder.Entity("Qb.Poco.Global.SensorType", b =>
+                {
+                    b.HasOne("Qb.Poco.Global.Parameter", "Parameter")
+                        .WithMany("SensorTypes")
+                        .HasForeignKey("ParameterId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("Qb.Poco.Global.Placement", "Placement")
+                        .WithMany("SensorTypes")
+                        .HasForeignKey("PlacementId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("Qb.Poco.Global.Subsystem", "Subsystem")
+                        .WithMany("SensorTypes")
+                        .HasForeignKey("SubsystemId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("Qb.Poco.User.CropCycle", b =>
+                {
+                    b.HasOne("Qb.Poco.Global.CropType", "CropType")
+                        .WithMany("CropCycles")
+                        .HasForeignKey("CropTypeName");
+
+                    b.HasOne("Qb.Poco.User.Location", "Location")
+                        .WithMany("CropCycles")
+                        .HasForeignKey("LocationId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("Qb.Poco.User.Device", b =>
+                {
+                    b.HasOne("Qb.Poco.User.Location", "Location")
+                        .WithMany("Devices")
+                        .HasForeignKey("LocationId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("Qb.Poco.User.Location", b =>
+                {
+                    b.HasOne("Qb.Poco.Person", "Person")
+                        .WithMany("Locations")
+                        .HasForeignKey("PersonId")
+                        .OnDelete(DeleteBehavior.SetNull);
+                });
+
+            modelBuilder.Entity("Qb.Poco.User.Sensor", b =>
+                {
+                    b.HasOne("Qb.Poco.User.Device", "Device")
+                        .WithMany("Sensors")
+                        .HasForeignKey("DeviceId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("Qb.Poco.Global.SensorType", "SensorType")
+                        .WithMany()
+                        .HasForeignKey("SensorTypeId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("Qb.Poco.User.SensorHistory", b =>
+                {
+                    b.HasOne("Qb.Poco.User.Location", "Location")
+                        .WithMany("SensorHistories")
+                        .HasForeignKey("LocationId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("Qb.Poco.User.Sensor", "Sensor")
+                        .WithMany("SensorHistories")
+                        .HasForeignKey("SensorId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
