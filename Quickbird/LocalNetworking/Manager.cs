6ba3d504f87e0dc32a1a117001f52a70a3a14e14
Vladimir Akopyan <Vladimir@akopyan.me>
2017-09-20 22:13:47 +0100

    Made code for managing JSON. Completed #28

--- Quickbird/LocalNetworking/Manager.cs
+++ Quickbird/LocalNetworking/Manager.cs
@@ -5,6 +5,7 @@
     using System.Diagnostics;
     using uPLibrary.Networking.M2Mqtt;
     using uPLibrary.Networking.M2Mqtt.Messages;
+    using Newtonsoft;
 
     /// <summary>This class is in charge of all the local communication. It initiates MQTT and UDP
     /// messaging. IF you try to instantiate this class twice, it will throw and exception!</summary>
@@ -98,39 +99,68 @@
             //TODO move this GUID parsing code somewhere appropriate!
             Guid clientID;
             var rawClientID = publishEvent.Key.Replace(":", string.Empty);
+
             if (Guid.TryParse(rawClientID, out clientID) == false)
             {
                 Util.Toast.NotifyUserOfError($"recieved message with invalid ClientID, {clientID} is not a valid ID");
+                return;
+            }
+
+            SensorMessage[] readings = null;
+            var message = publishEvent.Value;
+
+            if (message.Topic.Substring(0, 7).CompareTo("readings") != 0)
+                return;
+
+            else if (message.Topic.CompareTo("readings/v1/binary") == 0)
+                readings = DecodeBinaryMessage(message.Message);
+            else if (message.Topic.CompareTo("readings/v1/JSON") == 0)
+                readings = DecodeJSONMessage(message.Message); 
+
+            var toWrite = new KeyValuePair<Guid, SensorMessage[]>(clientID, readings);
+
+            if (readings != null)
+                _datapointsSaver.BufferAndSendReadings(toWrite);
+        }
+
+        private SensorMessage[] DecodeBinaryMessage(byte[] rawData)
+        {
+            SensorMessage[] readings = null;
+
+            if (rawData.Length % SensorMessage.incomingLength != 0)
+            {
+                Util.Toast.NotifyUserOfError($"message recieved over MQTT is invalid. it must consist of sensor readings, " +
+                    $"{SensorMessage.incomingLength} bytes long each. It's length is {rawData.Length}, not divisble by {SensorMessage.incomingLength}");
             }
             else
             {
-                SensorMessage[] readings;
-                var message = publishEvent.Value;
-
-                if (message.Topic.Contains("reading"))
+                var numberOfReadings = rawData.Length / SensorMessage.incomingLength;
+                readings = new SensorMessage[numberOfReadings];
+                //process each reading
+                for (var i = 0; i < numberOfReadings; i++)
                 {
-                    var rawData = message.Message;
-                    if (rawData.Length%SensorMessage.incomingLength != 0)
-                    {
-                        Util.Toast.NotifyUserOfError($"message recieved over MQTT is invalid. it must consist of sensor readings, {SensorMessage.incomingLength} bytes long each. It's length is {rawData.Length}, not divisble by {SensorMessage.incomingLength}");
-                    }
-                    else
-                    {
-                        var numberOfReadings = rawData.Length/SensorMessage.incomingLength;
-                        readings = new SensorMessage[numberOfReadings];
-                        //process each reading
-                        for (var i = 0; i < numberOfReadings; i++)
-                        {
-                            readings[i].value = BitConverter.ToSingle(rawData, i*SensorMessage.incomingLength);
-                            readings[i].duration = BitConverter.ToInt32(rawData, i*SensorMessage.incomingLength + 4);
-                            readings[i].SensorTypeID = rawData[i*SensorMessage.incomingLength + 8];
-                        }
-                        var toWrite = new KeyValuePair<Guid, SensorMessage[]>(clientID, readings);
-
-                        _datapointsSaver.BufferAndSendReadings(toWrite);
-                    }
+                    readings[i].value = BitConverter.ToSingle(rawData, i * SensorMessage.incomingLength);
+                    readings[i].duration = BitConverter.ToInt32(rawData, i * SensorMessage.incomingLength + 4);
+                    readings[i].SensorTypeID = rawData[i * SensorMessage.incomingLength + 8];
                 }
             }
+
+            return readings; 
+        }
+
+        private SensorMessage[] DecodeJSONMessage(byte[] rawData)
+        {
+            SensorMessage[] message = null; 
+            try
+            {
+                string messageSent = System.Text.Encoding.UTF8.GetString(rawData);
+                message = Newtonsoft.Json.JsonConvert.DeserializeObject<SensorMessage[]>(messageSent);
+            }
+            catch
+            {
+                Util.Toast.NotifyUserOfError($"message recieved over MQTT is not valid JSON. it must consist of sensor readings");
+            }
+            return message;
         }
 
         // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
